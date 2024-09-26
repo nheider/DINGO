635560a048bf325ae72d01dfa9c2dbaa6ccc3da9
user <heider@wifa.uni-leipzig.de>
2024-09-26 13:15:52 +0200

    .

--- start_com.bash
+++ start_com.bash
@@ -2,13 +2,10 @@
 
 
 # Open a new terminal and start XRCE_Agent 
-gnome-terminal --tab --command="bash -c 'MicroXRCEAgent serial --dev /dev/ttyUSB0 -b 921600'"
+gnome-terminal --tab --command="bash -c 'MicroXRCEAgent serial --dev /dev/ttyTHS1 -b 921600'"
 
 
 # Open another terminal and source PX4 ros WS 
 gnome-terminal --tab --command="bash -c 'cd ~/ws_sensor_combined; source install/setup.bash; echo \"~~~~ ros2 px4 ws sourced here ~~~~\"; bash'"
 
 
- 
-
-#& source install/setup.bash"
